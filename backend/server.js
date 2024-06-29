3be3db4e9cd480d7b53795977f552ed54534749e
jemoh002 <jamesnyajuaya@gmail.com>
2024-06-29 09:11:19 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
@@ -10,6 +11,11 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/api/user", userRoutes);
 
 app.use(notFound);
